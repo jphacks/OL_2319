3c2ead3a714ee2920d8a324344eaf2de8f30b86c
kuu13580 <13580kuu@gmail.com>
2023-10-26 02:10:05 +0900

    add 左サイドバーレイアウト

--- front/src/components/chat/ChatLeftSideBar.tsx
+++ front/src/components/chat/ChatLeftSideBar.tsx
@@ -2,11 +2,16 @@ export const ChatLeftSideBar = (props: { className?: string }) => {
   const { className } = props;
   return (
     <>
-      <div className={`channel-left-bar ${className}`}>
-        左サイドバー
+      <div className={`channel-left-bar text-center bg-primary d-flex flex-column align-items-center ${className}`}>
+        <button type="button" className="btn btn-back mt-7">
+          <img src="/arrow-back.svg" alt="back" width={40}/>
+        </button>
+        <div className="user-avater mt-7">
+          <img src="http://placekitten.com/200" alt="user avater" width={150} />
+        </div>
         <button
           type="button"
-          className="btn btn-outline-dark"
+          className="btn btn-outline-dark bg-white fw-bold fs-6 px-4 py-2 mt-7"
           data-bs-toggle="modal"
           data-bs-target="#modal-question"
         >
