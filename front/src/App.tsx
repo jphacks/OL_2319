04226cba00590fa41921015d76afd1f7709a29e6
kuu13580 <13580kuu@gmail.com>
2023-10-27 02:17:39 +0900

    add 参加グループ一覧画面

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/CustomBootstrap.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Top, SignUp, Login, Select, Channel } from "./pages";
+import { Top, SignUp, Login, Select, Channel, Group } from "./pages";
 import "./App.scss";
 
 function App() {
@@ -10,11 +10,12 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Top />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/select" element={<Select />} />
           <Route path="/channel/:chennelId" element={<Channel />} />
+          <Route path="/group" element={<Group />} />
+          <Route path="/*" element={<Top />} />
         </Routes>
       </Router>
     </>
--- front/src/pages/index.ts
+++ front/src/pages/index.ts
@@ -3,3 +3,4 @@ export { Top } from "./Top";
 export { Login } from "./Login";
 export { Select } from "./Select";
 export { Channel } from "./Channel";
+export { Group } from "./Group";
