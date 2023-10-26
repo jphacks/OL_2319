00dad9928bdb9ab669735b27a8d1bb1f7921451e
kuu13580 <13580kuu@gmail.com>
2023-10-27 03:07:02 +0900

    add ログイン時ヘッダー

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { UserSettingsModal } from ".";
 
 export const Header = () => {
+  const isLogin = true;
   return (
     <div className="header bg-gray-700">
       <div className="container d-flex justify-content-between align-items-center py-3">
@@ -16,28 +17,45 @@ export const Header = () => {
             <Link to="/select">チャンネルをさがす</Link>
           </div>
         </div>
-        <div className="header-right d-flex align-items-center">
-          <div className="login ms-3">
-            <Link to="/login">
-              <button
-                type="button"
-                className="btn btn-outline-primary fw-bold px-4 py-3 fs-5 lh-1"
-              >
-                ログイン
-              </button>
-            </Link>
+        {!isLogin && (
+          <div className="header-right d-flex align-items-center">
+            <div className="login ms-3">
+              <Link to="/login">
+                <button
+                  type="button"
+                  className="btn btn-outline-primary fw-bold px-4 py-3 fs-5 lh-1"
+                >
+                  ログイン
+                </button>
+              </Link>
+            </div>
+            <div className="signup ms-3">
+              <Link to="/signup">
+                <button
+                  type="button"
+                  className="btn btn-primary fw-bold px-4 py-3 fs-5 lh-1"
+                >
+                  新規登録
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="signup ms-3">
-            <Link to="/signup">
-              <button
-                type="button"
-                className="btn btn-primary fw-bold px-4 py-3 fs-5 lh-1"
-              >
-                新規登録
-              </button>
-            </Link>
+        )}
+        {isLogin && (
+          <div className="header-right d-flex align-items-center">
+            <div
+              className="user-avater btn"
+              data-bs-toggle="modal"
+              data-bs-target="#modal-user-settings"
+            >
+              <img
+                src="http://placekitten.com/200"
+                alt="user avater"
+                width={50}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <UserSettingsModal />
     </div>
