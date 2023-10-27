bce0217c90e5ad497b7c9b17bdcfd518c0df8eec
Rininmmmment <rinr10693otes@gmail.com>
2023-10-27 15:45:18 +0900

    add: タグ編集機能

--- server/app/controllers/tag_controller.rb
+++ server/app/controllers/tag_controller.rb
@@ -3,11 +3,28 @@ class TagController < ApplicationController
     @tag = Tag.new(
       name: params[:name],
     )
-    
     if @tag.save
-      render json: { status: 201 }
+      render json: @tag, status: 200
     else
-      render json: { status: 200 }
+      render status: 422
+    end
+  end
+
+  def get_all
+    @tags = Tag.all
+    if @tags.present?
+      render json: { tags: @tags }, status: 200
+    else
+      render status: 404
+    end
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(name: params[:name])
+      render json: @tag, status: 200
+    else
+      render status: 422
     end
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post 'server/tag-rel/create' => 'tag_rel#create'
+
   post 'server/tag/create' => 'tag#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'server/tag/get-all' => 'tag#get_all'
+  post 'server/tag/update' => 'tag#update'
 end
