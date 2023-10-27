e43ac99e5a59864a024109383ba6f1d4c2695b4e
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-10-27 15:08:06 +0900

    this was created when generate controller Users

--- server/test/controllers/users_controller_test.rb
+++ server/test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+  test "should get new" do
+    get users_new_url
     assert_response :success
   end
 
