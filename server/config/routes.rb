c27f954458c82e83382fceefc804ccdd41edd852
Rininmmmment <rinr10693otes@gmail.com>
2023-10-27 16:02:23 +0900

    fix: ルーティング

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  # tag_rel
   post 'server/tag-rel/create' => 'tag_rel#create'
 
+  # tag
   post 'server/tag/create' => 'tag#create'
+  get 'server/tag/get-all' => 'tag#get_all'
+  post 'server/tag/update' => 'tag#update'
+
+  # channel
   post "server/channels/create" => "channels#create"
   delete "server/channels/delete/:id" => "channels#delete"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
+
