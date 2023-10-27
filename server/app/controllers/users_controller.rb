8fcfb1768a1251b93712d05f0914508169ea80fa
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-10-27 15:01:34 +0900

    add functions for signup

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+
   def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if user.save
+      render status: 201
+    else
+      render json: { errors: "failed to create your account" }, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
