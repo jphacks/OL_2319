1db25549cb7ca0543e2a1f212d47ddbf34aab929
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-10-27 15:06:36 +0900

    fix data type of password from text into string

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_25_173914) do
+ActiveRecord::Schema.define(version: 2023_10_27_060449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +64,11 @@ ActiveRecord::Schema.define(version: 2023_10_25_173914) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.text "password", null: false
     t.text "icon"
     t.boolean "is_deleted", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
